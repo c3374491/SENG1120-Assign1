42706b0f81f2204271682df05e5f6a4f9d67c523
c3374491 <83261696+c3374491@users.noreply.github.com>
2022-03-16 21:42:24 +1100

    Initial Files for Project
    This file upload includes the following files:
    LinkedList.cpp
    LinkedListDemo.cpp (Provided with assignment)
    makefile (Provided with assignment)
    Node.cpp
    Student.cpp (Provided with assignment)
    Student.h (Provided with assignment)

---
+++ LinkedList.cpp
@@ -0,0 +1,5 @@
+// LinkedList.cpp
+/******************************/
+// Author: c3374491 | Ethan Davey
+// Course: SENG1120
+// File Description: This file outlines the creation and management of the linked list.
---
+++ Node.cpp
@@ -0,0 +1,5 @@
+// Node.cpp
+/******************************/
+// Author: c3374491 | Ethan Davey
+// Course: SENG1120
+// File Description: This file is used for the storage of the data types used in the linked list?
